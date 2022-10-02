585348db4be0823d476fb20ba025903b9c28e1f8
trichoip <107303342+trichoip@users.noreply.github.com>
2022-10-02 19:35:44 +0700

    change name api auth

--- eTransportationSystem/src/main/java/com/etransportation/controller/AuthController.java
+++ eTransportationSystem/src/main/java/com/etransportation/controller/AuthController.java
@@ -5,6 +5,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.etransportation.payload.request.LoginRequest;
@@ -12,7 +13,8 @@ import com.etransportation.payload.request.RegisterRequest;
 import com.etransportation.payload.response.LoginResponse;
 import com.etransportation.service.AccountService;
 
-@RestController("/api/auth")
+@RestController
+@RequestMapping("/api/auth")
 public class AuthController {
 
     @Autowired
