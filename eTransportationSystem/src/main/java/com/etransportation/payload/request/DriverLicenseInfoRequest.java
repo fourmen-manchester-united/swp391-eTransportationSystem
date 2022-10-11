d43568bd6b7ed2b4c50f572d8f0076bcbd28df0c
trichoip <107303342+trichoip@users.noreply.github.com>
2022-10-11 14:22:06 +0700

    fix date update drinving license

--- eTransportationSystem/src/main/java/com/etransportation/payload/request/DriverLicenseInfoRequest.java
+++ eTransportationSystem/src/main/java/com/etransportation/payload/request/DriverLicenseInfoRequest.java
@@ -2,6 +2,8 @@ package com.etransportation.payload.request;
 
 import java.util.Date;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+
 import lombok.Data;
 
 @Data
@@ -10,6 +12,8 @@ public class DriverLicenseInfoRequest {
     private Long account_Id;
     private Long numberDrivingLicense;
     private String name;
+
+    @JsonFormat(pattern = "yyyy-MM-dd")
     private Date birthDate;
     private String imageFront;
 }
