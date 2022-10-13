4a52f1c1223754a0fd1cf628f25df7b9b31e3d3b
trichoip <107303342+trichoip@users.noreply.github.com>
2022-10-14 03:37:59 +0700

    fix bug save car image

--- eTransportationSystem/src/main/java/com/etransportation/payload/dto/CarImageDTO.java
+++ eTransportationSystem/src/main/java/com/etransportation/payload/dto/CarImageDTO.java
@@ -1,5 +1,7 @@
 package com.etransportation.payload.dto;
 
+import javax.validation.constraints.NotEmpty;
+
 import org.hibernate.validator.constraints.Length;
 
 import lombok.Data;
@@ -8,5 +10,6 @@ import lombok.Data;
 public class CarImageDTO {
 
     @Length(min = 5)
+    @NotEmpty(message = "Image is required")
     private String image;
 }
