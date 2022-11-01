aac731e505529615b1899bf7d111d36651d5135a
trichoip <107303342+trichoip@users.noreply.github.com>
2022-11-01 09:43:00 +0700

    fix bug filter car

--- eTransportationSystem/src/main/java/com/etransportation/repository/CarRepository.java
+++ eTransportationSystem/src/main/java/com/etransportation/repository/CarRepository.java
@@ -17,32 +17,32 @@ import com.etransportation.payload.dto.CarModelDTO;
 @Repository
 public interface CarRepository extends JpaRepository<Car, Long>, JpaSpecificationExecutor<Car> {
 
-    List<Car> findAllByStatusAndAddress_City_Code(CarStatus status, String code, Pageable pageable);
+        List<Car> findAllByStatusAndAddress_City_Code(CarStatus status, String code, Pageable pageable);
 
-    List<Car> findAllByAccount_Id(Long id);
+        List<Car> findAllByAccount_Id(Long id);
 
-    Page<Car> findAllByStatus(CarStatus status, Pageable pageable);
+        Page<Car> findAllByStatus(CarStatus status, Pageable pageable);
 
-    @Query("SELECT new com.etransportation.payload.dto.CarBrandDTO(br.id, br.name, count(br.id))"
-            + " FROM Car c JOIN c.address a JOIN a.city ci JOIN c.model mo JOIN mo.brand br JOIN c.features f"
-            + " WHERE ci.id = ?1 AND c.status = ?2 AND c.price BETWEEN ?3 AND ?4 AND c.transmission like ?5"
-            + " AND c.fuel like ?6 AND c.seats in ?7 AND c.yearOfManufacture BETWEEN ?8 AND ?9"
-            + " AND f.id in ?10"
-            + " GROUP BY br.id, br.name "
-            + " ORDER BY count(br.id) DESC")
-    List<CarBrandDTO> findAllBrandByAddressCityIdAndCarStatus(Long id, CarStatus status, double min, double max,
-            String transmission, String fuel, Integer[] seats, int minyear, int maxyear, Long[] feature_Id);
+        @Query("SELECT new com.etransportation.payload.dto.CarBrandDTO(br.id, br.name, count(br.id))"
+                        + " FROM Car c JOIN c.address a JOIN a.city ci JOIN c.model mo JOIN mo.brand br JOIN c.features f"
+                        + " WHERE ci.id = ?1 AND c.status = ?2 AND c.price BETWEEN ?3 AND ?4 AND c.transmission like ?5"
+                        + " AND c.fuel like ?6 AND c.seats in ?7 AND c.yearOfManufacture BETWEEN ?8 AND ?9"
+                        + " AND f.id in ?10"
+                        + " GROUP BY br.id, br.name "
+                        + " ORDER BY count(br.id) DESC")
+        List<CarBrandDTO> findAllBrandByAddressCityIdAndCarStatus(Long id, CarStatus status, double min, double max,
+                        String transmission, String fuel, Integer[] seats, int minyear, int maxyear, Long[] feature_Id);
 
-    @Query("SELECT new com.etransportation.payload.dto.CarModelDTO(mo.id, mo.name, count(mo.id))"
-            + " FROM Car c JOIN c.address a JOIN a.city ci JOIN c.model mo JOIN mo.brand br JOIN c.features f"
-            + " WHERE ci.id = ?1 AND c.status = ?2 AND c.price BETWEEN ?3 AND ?4 AND c.transmission like ?5"
-            + " AND c.fuel like ?6 AND c.seats in ?7 AND c.yearOfManufacture BETWEEN ?8 AND ?9"
-            + " AND f.id in ?10"
-            + " GROUP BY mo.id, mo.name, br.id"
-            + " HAVING br.id = ?11"
-            + " ORDER BY count(mo.id) DESC")
-    List<CarModelDTO> findAllModelByAddressCityIdAndCarStatus(Long id, CarStatus status, double min, double max,
-            String transmission, String fuel, Integer[] seats, int minyear, int maxyear, Long[] feature_Id,
-            Long brand_Id);
+        @Query("SELECT new com.etransportation.payload.dto.CarModelDTO(mo.id, mo.name, count(mo.id))"
+                        + " FROM Car c JOIN c.address a JOIN a.city ci JOIN c.model mo JOIN mo.brand br JOIN c.features f"
+                        + " WHERE ci.id = ?1 AND c.status = ?2 AND c.price BETWEEN ?3 AND ?4 AND c.transmission like ?5"
+                        + " AND c.fuel like ?6 AND c.seats in ?7 AND c.yearOfManufacture BETWEEN ?8 AND ?9"
+                        + " AND f.id in ?10"
+                        + " GROUP BY mo.id, mo.name, br.id"
+                        + " HAVING br.id = ?11"
+                        + " ORDER BY count(mo.id) DESC")
+        List<CarModelDTO> findAllModelByAddressCityIdAndCarStatus(Long id, CarStatus status, double min, double max,
+                        String transmission, String fuel, Integer[] seats, int minyear, int maxyear, Long[] feature_Id,
+                        Long brand_Id);
 
 }
--- eTransportationSystem/src/main/java/com/etransportation/service/impl/CarServiceImpl.java
+++ eTransportationSystem/src/main/java/com/etransportation/service/impl/CarServiceImpl.java
@@ -328,6 +328,10 @@ public class CarServiceImpl implements CarService {
 
                 IntSummaryStatistics tt = IntStream.of(ArrayUtils.toPrimitive(filter.getYearOfManufactureBetween()))
                                 .summaryStatistics();
+                if (filter.getFeature_Id_in() == null || filter.getFeature_Id_in().length == 0) {
+                        filter.setFeature_Id_in(new Long[] { 1L, 2L, 3L, 4L, 5L, 6L, 7L, 8L, 9L, 10L, 11L, 12L, 13L,
+                                        14L, 15L, 16L, 17L, 18L });
+                }
 
                 PagingResponse<CarShortInfoResponse> pagingResponse = PagingResponse
                                 .<CarShortInfoResponse>builder()
